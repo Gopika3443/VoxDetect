574e119e40afc2254acad56d6fb2a509fcea4b82
Gopikav <gopikaprakash2002@gmail.com>
2023-12-09 12:07:03 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Front from './components/Front';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          welcome <code>src/App.js</code> to our page.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          VoxDetect
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <Routes>
+        <Route path='/' element={<Front/>}></Route>
+      </Routes>
+    </Router>
+
   );
 }
 
---
+++ src/components/Front.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Front.css';
+const Front=()=>{
+    return(
+        <div className="background">
+            <h1>VOXDETECT</h1>
+        </div>
+    );
+}
+export default Front;
